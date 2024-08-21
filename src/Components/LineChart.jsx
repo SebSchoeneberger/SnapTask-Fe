35ee7637d232d3c8eeb5e26645075a9ffa9f454e
hexzoner <hexzon@gmail.com>
2024-08-21 10:52:03 +0200

    updated dashboard with charts

--- src/Components/LineChart.jsx
+++ src/Components/LineChart.jsx
@@ -1,20 +1,29 @@
 import React, { useContext } from "react";
-import { Line, Doughnut } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 import { Chart as ChartJS, LineElement, CategoryScale, LinearScale, PointElement, Tooltip, Legend } from "chart.js";
 import { ColorContext } from "../Context/ColorProvider";
 
 // Register the required components with Chart.js
 ChartJS.register(LineElement, CategoryScale, LinearScale, PointElement, Tooltip, Legend);
 
-export default function LineChart() {
+export default function LineChart({ tasksPerDay }) {
   const { colors } = useContext(ColorContext);
+  tasksPerDay = {
+    "15.08.2024": 25,
+    "16.08.2024": 59,
+    "17.08.2024": 30,
+    "18.08.2024": 71,
+    "19.08.2024": 56,
+    "20.08.2024": 25,
+    "21.08.2024": 10,
+  };
 
   const data = {
-    labels: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+    labels: Object.keys(tasksPerDay),
     datasets: [
       {
-        label: "Tasks completed",
-        data: [25, 59, 30, 71, 56, 25, 10],
+        label: "Completed tasks",
+        data: Object.values(tasksPerDay),
         fill: true,
         backgroundColor: colors.secondary,
         hoverBackgroundColor: colors.primary,
@@ -25,6 +34,19 @@ export default function LineChart() {
         borderColor: colors.secondary,
         tension: 0.1,
       },
+      // {
+      //   label: "Overdue Tasks",
+      //   data: [15, 9, 10, 1, 6, 5, 0],
+      //   fill: true,
+      //   backgroundColor: colors.accent,
+      //   hoverBackgroundColor: colors.accent,
+      //   // pointBackgroundColor: colors.accent,
+      //   pointBorderColor: colors.accent,
+      //   pointRadius: 5,
+      //   pointHoverRadius: 8,
+      //   borderColor: colors.accent,
+      //   tension: 0.1,
+      // },
     ],
   };
 
