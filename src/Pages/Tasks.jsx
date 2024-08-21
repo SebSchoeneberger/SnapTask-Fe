0ddc43976491a452483d670a1d49bb4727a4209c
Michal <michalochecki00@gmail.com>
2024-08-21 18:27:26 +0200

    update

--- src/Pages/Tasks.jsx
+++ src/Pages/Tasks.jsx
@@ -1,30 +1,12 @@
-import React from 'react'; 
-import { useState } from 'react';
-import CreateTask from '../Components/CreateTask';
-
+import React from 'react';
+import TasksList from '../Components/TasksList'; 
 
 const Tasks = () => {
-  
-const [isOpen, setIsOpen] = useState (false)
-const onClose = () => {setIsOpen (false)}
-  
-
- 
-
-
-  return (
-    <div className="p-8 w-full flex flex-col items-center">
-      <h1 className="text-2xl font-bold">Task Management</h1>
-      <button 
-        onClick={() => { setIsOpen(true); }} 
-        className="btn btn-outline btn-primary mt-4">
-        Create New Task
-      </button>
-      <CreateTask isOpen={isOpen} onClose={onClose}/>
-    </div>
-  );
-}
-       
- 
+    return (
+        <div className="p-8 w-full flex flex-col items-center">
+            <TasksList />
+        </div>
+    );
+};
 
 export default Tasks;
