801151e41515a13c6c419157f3efa21b06237540
hexzoner <hexzon@gmail.com>
2024-08-29 11:16:24 +0200

    multiselect dropdown polishing

--- src/Components/MutiselectComponent.jsx
+++ src/Components/MutiselectComponent.jsx
@@ -1,26 +1,21 @@
-import React, { useEffect, useState } from "react";
 import Multiselect from "multiselect-react-dropdown";
 import Select from "react-select";
 
-function MultiselectComponent({ users = [] }) {
-  const [options, setOptions] = useState([]);
-
-  console.log(options);
-
-  useEffect(() => {
-    const userOptions = users.map((user) => {
-      return { label: user.firstName, value: user._id };
-    });
-    console.log(options);
-    console.log(userOptions);
-
-    setOptions(userOptions);
-  }, []);
-
+function MultiselectComponent({ users }) {
   return (
-    <>
-      <Select options={options} isMulti />
-    </>
+    <div className="w-full">
+      <Select
+        options={users.map((user) => {
+          let label = "";
+          if (user.firstName) label += user.firstName;
+          if (user.lastName) label += " " + user.lastName;
+          if (label.length === 0) label = user.email;
+          else label += " - " + user.email;
+          return { label, value: user._id };
+        })}
+        isMulti
+      />
+    </div>
   );
 }
 
