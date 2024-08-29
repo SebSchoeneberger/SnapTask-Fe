0629e31937d33be5677cccb93abb0d90f1ade23e
hexzoner <hexzon@gmail.com>
2024-08-29 23:05:34 +0200

    fix merging

--- src/Components/TasksList.jsx
+++ src/Components/TasksList.jsx
@@ -23,6 +23,7 @@ const TasksList = () => {
   const [tasks, setTasks] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
   const [editTask, setEditTask] = useState(null);
+  const [taskUsers, setTaskUsers] = useState([]);
   const [deleteTask, setDeleteTask] = useState(null);
   const [loading, setLoading] = useState(true);
   const [selectedTask, setSelectedTask] = useState(null);
@@ -54,6 +55,7 @@ const TasksList = () => {
   }, [tasksUrl, token, page, perPage]);
 
   const handleEdit = (task) => {
+    setTaskUsers(task.assignedTo);
     setEditTask(task);
   };
 
@@ -155,23 +157,15 @@ const TasksList = () => {
                 <th>
                   <div className="flex gap-1 items-center">
                     <span>Task Name</span>
-                    <button
-                      className="hover:cursor-pointer"
-                      onClick={() => handleSortClick("title")}
-                    >
+                    <button className="hover:cursor-pointer" onClick={() => handleSortClick("title")}>
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
                         strokeWidth="1.5"
                         stroke="currentColor"
-                        className="size-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
-                        />
+                        className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                       </svg>
                     </button>
                   </div>
@@ -179,23 +173,15 @@ const TasksList = () => {
                 <th>
                   <div className="flex gap-1 items-center">
                     <span>Description</span>
-                    <button
-                      className="hover:cursor-pointer"
-                      onClick={() => handleSortClick("description")}
-                    >
+                    <button className="hover:cursor-pointer" onClick={() => handleSortClick("description")}>
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
                         strokeWidth="1.5"
                         stroke="currentColor"
-                        className="size-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
-                        />
+                        className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                       </svg>
                     </button>
                   </div>
@@ -203,23 +189,15 @@ const TasksList = () => {
                 <th>
                   <div className="flex gap-1 items-center">
                     <span>Due Date</span>
-                    <button
-                      className="hover:cursor-pointer"
-                      onClick={() => handleSortClick("dueDate")}
-                    >
+                    <button className="hover:cursor-pointer" onClick={() => handleSortClick("dueDate")}>
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
                         strokeWidth="1.5"
                         stroke="currentColor"
-                        className="size-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
-                        />
+                        className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                       </svg>
                     </button>
                   </div>
@@ -228,23 +206,15 @@ const TasksList = () => {
                 <th>
                   <div className="flex gap-1 items-center">
                     <span>Status</span>
-                    <button
-                      className="hover:cursor-pointer"
-                      onClick={() => handleSortClick("status")}
-                    >
+                    <button className="hover:cursor-pointer" onClick={() => handleSortClick("status")}>
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
                         strokeWidth="1.5"
                         stroke="currentColor"
-                        className="size-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
-                        />
+                        className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                       </svg>
                     </button>
                   </div>
@@ -253,23 +223,15 @@ const TasksList = () => {
                 <th>
                   <div className="flex gap-1 items-center">
                     <span>Priority</span>
-                    <button
-                      className="hover:cursor-pointer"
-                      onClick={() => handleSortClick("priority")}
-                    >
+                    <button className="hover:cursor-pointer" onClick={() => handleSortClick("priority")}>
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
                         strokeWidth="1.5"
                         stroke="currentColor"
-                        className="size-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
-                        />
+                        className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                       </svg>
                     </button>
                   </div>
@@ -277,23 +239,15 @@ const TasksList = () => {
                 <th>
                   <div className="flex gap-1 items-center">
                     <span>Assigned to</span>
-                    <button
-                      className="hover:cursor-pointer"
-                      onClick={() => handleSortClick("assignedTo.firstName")}
-                    >
+                    <button className="hover:cursor-pointer" onClick={() => handleSortClick("assignedTo.firstName")}>
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
                         strokeWidth="1.5"
                         stroke="currentColor"
-                        className="size-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
-                        />
+                        className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                       </svg>
                     </button>
                   </div>
@@ -301,23 +255,15 @@ const TasksList = () => {
                 <th>
                   <div className="flex gap-1 items-center">
                     <span>Area</span>
-                    <button
-                      className="hover:cursor-pointer"
-                      onClick={() => handleSortClick("area.name")}
-                    >
+                    <button className="hover:cursor-pointer" onClick={() => handleSortClick("area.name")}>
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
                         fill="none"
                         viewBox="0 0 24 24"
                         strokeWidth="1.5"
                         stroke="currentColor"
-                        className="size-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9"
-                        />
+                        className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
                       </svg>
                     </button>
                   </div>
@@ -331,14 +277,10 @@ const TasksList = () => {
                   key={task._id}
                   className="hover:cursor-pointer hover:bg-base-200 h-28"
                   onClick={(e) => {
-                    if (
-                      !e.target.closest(".dropdown-content") &&
-                      !e.target.closest(".dropdown")
-                    ) {
+                    if (!e.target.closest(".dropdown-content") && !e.target.closest(".dropdown")) {
                       handleRowClick(task);
                     }
-                  }}
-                >
+                  }}>
                   <td className="font-bold">{index + 1}</td>
                   <td>{task.title}</td>
                   <td className="truncate-multiline ">{task.description}</td>
@@ -364,8 +306,7 @@ const TasksList = () => {
                           viewBox="0 0 24 24"
                           strokeWidth="1.5"
                           stroke="currentColor"
-                          className="w-6 h-6"
-                        >
+                          className="w-6 h-6">
                           <path
                             strokeLinecap="round"
                             strokeLinejoin="round"
@@ -379,8 +320,7 @@ const TasksList = () => {
                             onClick={(e) => {
                               e.stopPropagation();
                               handleEdit(task);
-                            }}
-                          >
+                            }}>
                             Edit
                           </button>
                         </li>
@@ -389,8 +329,7 @@ const TasksList = () => {
                             onClick={(e) => {
                               e.stopPropagation();
                               handleDelete(task);
-                            }}
-                          >
+                            }}>
                             Delete
                           </button>
                         </li>
@@ -401,57 +340,23 @@ const TasksList = () => {
               ))}
             </tbody>
           </table>
-          <Pagination
-            page={page}
-            setPage={setPage}
-            perPage={perPage}
-            setPerPage={setPerPage}
-            totalPages={totalPages}
-            totalResults={totalResults}
-          />
+          <Pagination page={page} setPage={setPage} perPage={perPage} setPerPage={setPerPage} totalPages={totalPages} totalResults={totalResults} />
         </div>
       ) : (
         <p>No tasks found.</p>
       )}
 
-      {modalOpen && !editTask && (
-        <CreateTask
-          isOpen={modalOpen}
-          onClose={() => setModalOpen(false)}
-          onCreate={updateTasks}
-        />
-      )}
-      {editTask && (
-        <EditTaskModal
-          taskData={editTask}
-          updateTasks={updateTasks}
-          onClose={() => setEditTask(null)}
-        />
-      )}
+      {modalOpen && !editTask && <CreateTask isOpen={modalOpen} onClose={() => setModalOpen(false)} onCreate={updateTasks} />}
+      {editTask && <EditTaskModal taskUsers={taskUsers} taskData={editTask} updateTasks={updateTasks} onClose={() => setEditTask(null)} />}
       <TaskDetailsPopup task={selectedTask} />
-      <dialog
-        id="delete_task_modal"
-        className="modal modal-bottom sm:modal-middle"
-      >
+      <dialog id="delete_task_modal" className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
           <button
             type="button"
             onClick={() => document.getElementById("delete_task_modal").close()}
-            className="btn btn-square absolute top-4 right-4"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M6 18L18 6M6 6l12 12"
-              />
+            className="btn btn-square absolute top-4 right-4">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
           <h3 className="font-bold text-lg">Delete Task</h3>
