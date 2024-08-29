8673cff67e9a273cc8ff4395877e5029ed80d8eb
Felipe Borja <165383150+felborja@users.noreply.github.com>
2024-08-29 14:17:46 +0200

    Multi select in Tasks

--- src/Components/MutiselectComponent.jsx
+++ src/Components/MutiselectComponent.jsx
@@ -1,11 +1,11 @@
 import Multiselect from "multiselect-react-dropdown";
 import Select from "react-select";
 
-
-function MultiselectComponent({ users }) {
+function MultiselectComponent({ users, setSelectedUsers }) {
   return (
     <div className="w-full">
       <Select
+        onChange={(value) => setSelectedUsers(value.map((user) => user.value))}
         options={users.map((user) => {
           let label = "";
           if (user.firstName) label += user.firstName;
