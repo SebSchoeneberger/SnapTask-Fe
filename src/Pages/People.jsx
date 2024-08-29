d4516ecb797897848165385ff07fad323f3342ca
Sebastian Schoeneberger <sebastian.schoeneberger@gmail.com>
2024-08-29 11:17:52 +0200

    Peoples Sorting function

--- src/Pages/People.jsx
+++ src/Pages/People.jsx
@@ -10,6 +10,7 @@ import { formatDateFull } from "../Utils/DateUtils";
 import Pagination from "../Components/Dashboard/Pagination";
 import defaultAvatar from "../assets/defaultAvatar.svg";
 import DefaultProfileImage from "../Components/DefaultProfileImage";
+import sortTables from "../Utils/SortTablesUtils";
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -18,6 +19,8 @@ export default function Users() {
   const [perPage, setPerPage] = useState(10);
   const [totalPages, setTotalPages] = useState(0);
   const [totalResults, setTotalResults] = useState(0);
+
+  const [sortOrder, setSortOrder] = useState("asc");
 
   const [users, setUsers] = useState([]);
   const [editUser, setEditUser] = useState(null);
@@ -104,6 +107,13 @@ export default function Users() {
     };
   }, []);
 
+  const handleSortClick = (key) => {
+    const newSortOrder = sortOrder === "asc" ? "desc" : "asc";
+    setSortOrder(newSortOrder);
+    const sortedAreas = sortTables(users, key, newSortOrder);
+    setUsers(sortedAreas);
+  };
+
   if (loading)
     return (
       <div className="min-h-screen w-full text-left px-12">
@@ -125,11 +135,56 @@ export default function Users() {
             <thead>
               <tr>
                 <th></th>
-                <th>Name</th>
-                <th>Email Address</th>
-                <th>Created by</th>
-                <th>Created at</th>
-                <th>Account Type</th>
+                <th>
+                <div className="flex gap-1 items-center">
+                  <span>Name</span>
+                  <button className="hover:cursor-pointer" onClick={() => handleSortClick("firstName")}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                    </svg>
+                  </button>
+                </div>
+              </th>
+              <th>
+                <div className="flex gap-1 items-center">
+                  <span>Email Adress</span>
+                  <button className="hover:cursor-pointer" onClick={() => handleSortClick("email")}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                    </svg>
+                  </button>
+                </div>
+              </th>
+              <th>
+                <div className="flex gap-1 items-center">
+                  <span>Created by</span>
+                  <button className="hover:cursor-pointer" onClick={() => handleSortClick("creator.firstName")}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                    </svg>
+                  </button>
+                </div>
+              </th>
+              <th>
+                <div className="flex gap-1 items-center">
+                  <span>Created at</span>
+                  <button className="hover:cursor-pointer" onClick={() => handleSortClick("createdAt")}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                    </svg>
+                  </button>
+                </div>
+              </th>
+              <th>
+                <div className="flex gap-1 items-center">
+                  <span>Account type</span>
+                  <button className="hover:cursor-pointer" onClick={() => handleSortClick("role")}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 15 12 18.75 15.75 15m-7.5-6L12 5.25 15.75 9" />
+                    </svg>
+                  </button>
+                </div>
+              </th>
               </tr>
             </thead>
             <tbody>
