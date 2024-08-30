fadf89f8a83e0edab14d2303cd992458a155f092
Felipe Borja <165383150+felborja@users.noreply.github.com>
2024-08-30 17:33:01 +0200

    small changes

--- src/Components/ThemesSwap.jsx
+++ src/Components/ThemesSwap.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useContext } from "react";
 import { ColorContext } from "../Context/ColorProvider";
 
 const storageKey = "snap-task-theme";
-export const themesSwap = ["cupcake", "dim"];
+export const themesSwap = ["light", "dim"];
 
 export default function ThemesSwap() {
   // const [theme, setTheme] = React.useState(
@@ -45,11 +45,17 @@ export default function ThemesSwap() {
         stroke="currentColor"
         strokeWidth="2"
         strokeLinecap="round"
-        strokeLinejoin="round">
+        strokeLinejoin="round"
+      >
         <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
       </svg>
 
-      <input type="checkbox" onChange={handleSwap} checked={theme === themesSwap[0] ? true : false} className="toggle theme-controller" />
+      <input
+        type="checkbox"
+        onChange={handleSwap}
+        checked={theme === themesSwap[0] ? true : false}
+        className="toggle theme-controller"
+      />
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="20"
@@ -59,7 +65,8 @@ export default function ThemesSwap() {
         stroke="currentColor"
         strokeWidth="2"
         strokeLinecap="round"
-        strokeLinejoin="round">
+        strokeLinejoin="round"
+      >
         <circle cx="12" cy="12" r="5" />
         <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
       </svg>
