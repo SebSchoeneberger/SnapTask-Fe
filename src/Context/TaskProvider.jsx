871127361dab32fa3f632bec751e128b8a8227ba
Felipe Borja <165383150+felborja@users.noreply.github.com>
2024-09-02 15:28:59 +0200

    ui update staff

--- src/Context/TaskProvider.jsx
+++ src/Context/TaskProvider.jsx
@@ -3,8 +3,12 @@ import React, { createContext, useState, useEffect } from "react";
 export const TaskContext = createContext();
 
 const TaskProvider = ({ children }) => {
-  const [area, setArea] = useState("All areas");
-  return <TaskContext.Provider value={{ area, setArea }}>{children}</TaskContext.Provider>;
+  const [area, setArea] = useState("All Areas");
+  return (
+    <TaskContext.Provider value={{ area, setArea }}>
+      {children}
+    </TaskContext.Provider>
+  );
 };
 
 export default TaskProvider;
