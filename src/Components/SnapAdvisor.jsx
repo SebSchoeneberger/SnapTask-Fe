dab1d5dbdb880354e218142c6b02e476ff88f18a
Sebastian Schoeneberger <sebastian.schoeneberger@gmail.com>
2024-09-03 11:13:49 +0200

    Ai bot bug fixes

--- src/Components/SnapAdvisor.jsx
+++ src/Components/SnapAdvisor.jsx
@@ -23,25 +23,31 @@ function SnapAdvisor() {
     'Authorization': `${apiToken}`,
   };
 
-  const body = {
-    "model": "gpt-3.5-turbo",
-    'response_format': { 'type': 'json_object' },
-    "messages": [
-      {
-        "role": "system",
-        "content": "You are Snap Advisor, a assistant for our SnapTask Taskmanagement App."
-      },
-      {
-        "role": "user",
-        "content": input,
-      }
-    ]
-  }
+  const handleSubmit = (message) => {
 
+    const userMessage = message || input;
+    
+    if (!userMessage) {
+      return;
+    }
 
-  const handleSubmit = () => {
-    const newMessages = [...messages, { role: "user", content: input }];
+    const newMessages = [...messages, { role: "user", content: userMessage }];
     setMessages(newMessages);
+
+    const body = {
+      "model": "gpt-3.5-turbo",
+      'response_format': { 'type': 'json_object' },
+      "messages": [
+        {
+          "role": "system",
+          "content": "You are Snap Advisor, a assistant for our SnapTask Taskmanagement App."
+        },
+        {
+          "role": "user",
+          "content": userMessage,
+        }
+      ]
+    }
 
     axios
       .post(`${API_URL}/chat/completion`, body, { headers })
@@ -66,7 +72,22 @@ function SnapAdvisor() {
         content: "Hey, this is your Snap Advisor! How can I help you today?",
       },
     ])
+    setInput("")
   }
+
+  const suggestedQuestions = [
+    "How can I generate and assign a QR code for a new task in a specific area?",
+    "How do I add or remove staff members and assign them tasks from the admin dashboard?",
+    "What’s the process for a staff member to start and complete a task using their phone?",
+    "Help me create a performance report for a specific staff member over the last month.",
+    "Can you help me analyze the performance trends of my staff over the past month?",
+    "Can you help me analyze the performance in a particular area?",
+  ];
+
+  const handleSuggestedQuestion = (question) => {
+    setInput(question);
+    handleSubmit(question);
+  };
 
   return (
     <>
@@ -109,67 +130,32 @@ function SnapAdvisor() {
               <div className="modal-action border-2 flex flex-col justify-start h-[600px] w-[400px]">
                 <h3 className="p-4">Suggested Questions:</h3>
                 <ul className="menu bg-base-200 rounded-box p-2 overflow-y-auto">
-                  <li>
-                    <a>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                      </svg>
-                      Question 1
-                    </a>
-                  </li>
-
-                  <li>
-                    <a>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                      </svg>
-                      Question 2
-                    </a>
-                  </li>
-
-                  <li>
-                    <a>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="2"
-                          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                      </svg>
-                      Question 3
-                    </a>
-                  </li>
-
+                {suggestedQuestions.map((question, index) => (
+                    <li key={index} >
+                      <a onClick={() => { handleSuggestedQuestion(question) }}>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor">
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                        {question}
+                      </a>
+                    </li>
+                  ))}
                 </ul>
               </div>
             </div>
 
             <label className="input input-bordered flex input-primary items-center w-[840px] gap-2">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
             </svg>
               <input
                 type="text"
